d734dae5ae3edb42aa931b76a6e1cd3cd6f97459
immortal1256 <65720840+immortal1256@users.noreply.github.com>
2021-09-03 08:41:41 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -224,8 +224,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
     elif query.data=="help":
         buttons = [
             [
-                InlineKeyboardButton('⚙️ Update Channel', url='https://t.me/subin_works'),
-                InlineKeyboardButton('🧩 Source', url='https://github.com/subinps/MusicPlayer'),
+                InlineKeyboardButton('⚙️ My Master', url='https://t.me/toxic_rahul'),
+                InlineKeyboardButton('🧩 Source', url='https://t.me/toxicrahul_musicbot'),
             ]
         ]
         reply_markup = InlineKeyboardMarkup(buttons)
